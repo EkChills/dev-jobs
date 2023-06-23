fa098009a37b79ac3b0ca5907f384a3454d253c8
Ekene <ekeneezeifeomasam27@gmail.com>
2023-06-23 21:18:56 +0100

    test

--- src/app/api/jobs/route.tsx
+++ src/app/api/jobs/route.tsx
@@ -2,7 +2,7 @@ import { NextApiRequest } from "next";
 import { prisma } from "../../../../prisma/prisma";
 import { NextResponse } from "next/server";
 import jobsData from "../../data.json";
-import { v4 as uuid } from "uuid";
+// import { v4 as uuid } from "uuid";
 import { Job } from "@prisma/client";
 import { verifyJwt } from "@/lib/jwt";
 
@@ -12,24 +12,24 @@ export async function GET(req: Request) {
     const lastId = searchParams.get('lastId')
     const searchTerm = searchParams.get('search')
 
-    // const headers = req.headers.get('Fazeportal')
-    // const verifiedToken = verifyJwt(headers!)
-    // if(!verifiedToken) {
-    //   return new NextResponse('unauthorized', {status:401})
-    // }
+    const headers = req.headers.get('Fazeportal')
+    const verifiedToken = verifyJwt(headers!)
+    if(!verifiedToken) {
+      return new NextResponse('unauthorized', {status:401})
+    }
   
     
-    // const jobs:Job[] | any = jobsData
-    const jobs:Job[] = await prisma.job.findMany({
-      take: 9,
-      skip:lastId ? 1 : 0,
-      cursor: lastId ? {
-        id:lastId!
-      } : undefined
-    });
+    const jobs:Job[] | any = jobsData
+    // const jobs:Job[] = await prisma.job.findMany({
+    //   take: 9,
+    //   skip:lastId ? 1 : 0,
+    //   cursor: lastId ? {
+    //     id:lastId!
+    //   } : undefined
+    // });
 
     if(searchTerm) {
-      const filteredJobs = jobs.filter((job) => job.position.toLowerCase().replace('-', '').startsWith(searchTerm.toLowerCase()))
+      const filteredJobs = jobs.filter((job:Job) => job.position.toLowerCase().replace('-', '').startsWith(searchTerm.toLowerCase()))
       console.log(jobs);
       console.log(filteredJobs); 
       console.log(searchTerm);
