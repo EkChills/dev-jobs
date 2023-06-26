39802d9942cd1c68cb4e12fd77739a4ee091c2db
Ekene <ekeneezeifeomasam27@gmail.com>
2023-06-26 17:11:58 +0100

    fixed client side issues

--- src/components/SearchFilters.tsx
+++ src/components/SearchFilters.tsx
@@ -2,17 +2,20 @@
 
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FilterModal from "./modals/FilterModal";
 import ReactDOM from "react-dom";
 
 export default function SearchFilters() {
   const { theme, systemTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState<boolean>(false)
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  console.log(isModalOpen);
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
 
   const currentTheme = theme === "system" ? systemTheme : theme;
-  console.log(currentTheme);
 
   return (
     <>
@@ -32,7 +35,7 @@ export default function SearchFilters() {
             <path
               d="M19.108 0H.86a.86.86 0 00-.764.455.833.833 0 00.068.884l6.685 9.202.007.01c.242.32.374.708.375 1.107v7.502a.825.825 0 00.248.594.865.865 0 00.942.18l3.756-1.4c.337-.1.56-.41.56-.784v-6.092c0-.399.132-.787.375-1.108l.007-.009 6.685-9.202c.19-.26.217-.6.068-.884A.86.86 0 0019.108 0z"
               className={`${
-                currentTheme === "dark" ? "fill-[#ffffff]" : "fill-[#6E8098]"
+                isMounted && currentTheme === "dark" ? "fill-[#ffffff]" : "fill-[#6E8098]"
               } hover:cursor-pointer`}
               // fill="#6E8098"
               // fill={`${currentTheme === "dark" ? "#ffffff" : "#6E8098"}`}
