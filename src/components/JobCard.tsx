d4e695130a8a53c302f6e3f3e6a2c04fcd5c7d8a
Ekene <ekeneezeifeomasam27@gmail.com>
2023-06-26 22:56:37 +0100

    debugging

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import React from 'react'
 import { motion } from 'framer-motion'
 import { useRouter } from 'next/navigation'
+import { baseUrl } from '@/utils/baseUrl'
 
 
 export default function JobCard({postedAt, contract, position, location, logo, company, logoBackground, id}:Job) {
@@ -18,7 +19,7 @@ export default function JobCard({postedAt, contract, position, location, logo, c
   console.log(logoBackground);
   
   return (
-    <motion.div  className='flex flex-col rounded-md px-8 pb-8 pt-[49px] bg-white dark:bg-[#19202D] cursor-pointer relative -z-1 hover:shadow-xl transition-shadow duration-500' initial={{opacity:0}} whileInView={{opacity:1}} onClick={() => router.push(`/jobs/${id}`)} >
+    <motion.div  className='flex flex-col rounded-md px-8 pb-8 pt-[49px] bg-white dark:bg-[#19202D] cursor-pointer relative -z-1 hover:shadow-xl transition-shadow duration-500' initial={{opacity:0}} whileInView={{opacity:1}} onClick={() => router.push(`${baseUrl}/jobs/${id}`)} >
       <div style={{backgroundColor:logoBackground}} className={`absolute top-[-1.5rem] left-[2rem] w-[3.125rem] h-[3.125rem] p-2 flex items-center justify-center rounded-xl bg-[${logoBackground.trim()}]`} >
       <Image src={logo} width={48} height={48} alt={company}  className='' />
       </div>
